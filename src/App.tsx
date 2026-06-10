f1528f547c137c59ed2c6bbb7901c346947000bc
Tatiana <tany663@gmail.com>
2026-06-10 20:46:10 +0300

    header update and styled

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ import HomePage from "./pages/HomePage/HomePage";
 import TeachersPage from "./pages/TeachersPage/TeachersPage";
 import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import { useState } from "react";
 
 export default function App() {
+  const [authMode, setAuthMode] = useState<AuthMode>(null);
+  console.log(authMode);
+
+  console.log(authMode);
   return (
     <>
-      <Header />
+      <Header
+        onLoginClick={() => setAuthMode("login")}
+        onRegisterClick={() => setAuthMode("register")}
+      />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/teachers" element={<TeachersPage />} />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,43 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import css from "./Header.module.css";
 
+interface HeaderProps {
+  onLoginClick: () => void;
+  onRegisterClick: () => void;
+}
 
-export default function Header() {
+export default function Header({ onLoginClick, onRegisterClick }: HeaderProps) {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/teachers">Teachers</Link>
-        <Link to="/favorites">Favorites</Link>
+     <header className={css.header}>
+      <NavLink to="/" className={css.logo}>
+        <span className={css.logoIcon}></span>
+        <span className={css.logoText}>LearnLingo</span>
+      </NavLink>
+
+      <nav className={css.nav}>
+        <NavLink to="/" className={css.navLink}>
+          Home
+        </NavLink>
+
+        <NavLink to="/teachers" className={css.navLink}>
+          Teachers
+        </NavLink>
       </nav>
+
+      <div className={css.auth}>
+        <button type="button" className={css.loginBtn} onClick={onLoginClick}>
+          <span className={css.loginIcon}>↪</span>
+          Log in
+        </button>
+
+        <button
+          type="button"
+          className={css.registerBtn}
+          onClick={onRegisterClick}
+        >
+          Registration
+        </button>
+      </div>
     </header>
   );
 }
